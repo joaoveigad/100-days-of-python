24382c15388b0147cf14743667c6c17018f7940a
João <jvdvandrade@outlook.pt>
2024-12-24 17:25:30 -0300

    day one!

--- day 1/day 1.py
+++ day 1/day 1.py
@@ -1,1 +1,4 @@
-print('Hello, World!')
+print('Welcome to your Band Name Generator.')
+cityName = input("What's the name of the city you grew up in? \n")
+petName = input("Cool! So, what's your pet's name? \n")
+print(f'Your band name is {cityName} {petName}!')
