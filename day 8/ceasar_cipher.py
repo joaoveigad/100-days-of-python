632047ef1fbd45ca66e47dcffdf674eea2896127
João <jvdvandrade@outlook.pt>
2025-01-03 15:11:03 -0300

    fixed a decrypting bug and added a loop to make sure the correct option gets selected

--- day 8/ceasar_cipher.py
+++ day 8/ceasar_cipher.py
@@ -1,22 +1,29 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-encode_or_decode = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
-text = input("Type your message! \n").lower()
-shift = int(input("Type the shift number: \n"))
 
-
-def cipher(original_text, shift_amount):
-    output = ''
+def cipher(original_text, shift_amount, encode_or_decode):
+    if direction != "encode" and direction != "decode":
+        print('Please type the correct option')
+        return
+    encrypted_text = ''
+    if encode_or_decode == "decode":
+        shift_amount *= -1
     for letter in original_text:
         if letter not in alphabet:
-            output += letter
+            encrypted_text += letter
         else:
-            if encode_or_decode == "decode":
-                shift_amount *= -1
-            new_index = alphabet.index(letter) + shift_amount
-            new_index %= len(alphabet)
-            output += alphabet[new_index]
-    print(output)
+            new_index = (alphabet.index(letter) + shift_amount) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    print(encrypted_text)
 
-cipher(text, shift)
+
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
+    if direction not in ["encode", "decode"]:
+        print("Please type the correct option!")
+        continue
+    text = input("Type your message! \n").lower()
+    shift = int(input("Type the shift number: \n"))
+    cipher(text, shift, direction)
+    break
